aecfd1d7b0875f559bf40b524ff7f6b491006941
thisellie <81550623+arnplsrz@users.noreply.github.com>
2025-06-26 18:15:53 +0800

    Add membership status to users table

--- database/schema.sql
+++ database/schema.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS users (
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   username VARCHAR(50) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  membership_status BOOLEAN NOT NULL DEFAULT FALSE,
 );
 
 CREATE TABLE IF NOT EXISTS posts (
