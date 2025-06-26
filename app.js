e3659c6b7b27f17dcb91f85e86004825f7e5ec12
thisellie <81550623+arnplsrz@users.noreply.github.com>
2025-06-26 18:17:29 +0800

    Import authentication packages

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+const path = require('node:path')
+const { Pool } = require('pg')
 const express = require('express')
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 const indexRoute = require('./routes/indexRoute')
 
 require('dotenv').config()
@@ -10,7 +15,9 @@ app.set('views', `${__dirname}/views`)
 
 app.use(express.static('public'))
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }))
+app.use(passport.session())
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/', indexRoute)
 
