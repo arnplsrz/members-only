f37d28859a493f1608aee245a2b5a3ab5923fdde
Aaron Paul Suarez <81550623+arnplsrz@users.noreply.github.com>
2026-07-25 14:19:12 +0800

    chore(cfg): use connectionString

--- database/pool.js
+++ database/pool.js
@@ -1,12 +1,4 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-};
-
-module.exports = new Pool(config);
+module.exports = new Pool({ connectionString: process.env.DATABASE_URL });
