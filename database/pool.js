fe53234e83ce68f02e8c24a3d97e9a1d649790ad
Aaron Paul Suarez <81550623+arnplsrz@users.noreply.github.com>
2026-07-25 14:02:32 +0800

    refactor: remove ssl certificate

--- database/pool.js
+++ database/pool.js
@@ -1,10 +1,5 @@
 const { Pool } = require("pg");
-const fs = require("fs");
-const path = require("path");
 require("dotenv").config();
-
-const certPath = path.join(__dirname, "../global-bundle.pem");
-const ca = fs.readFileSync(certPath).toString().split(/(?=-----BEGIN CERTIFICATE-----)/);
 
 const config = {
   user: process.env.DB_USER,
@@ -12,10 +7,6 @@ const config = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: true,
-    ca,
-  },
 };
 
 module.exports = new Pool(config);
